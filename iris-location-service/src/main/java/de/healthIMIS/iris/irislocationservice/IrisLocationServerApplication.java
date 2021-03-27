057578432676becd51647767aa6beb89e9a35905
Kai Greshake <development@kai-greshake.de>
2021-03-27 11:36:47 +0100

    Initialize location-service

---
+++ iris-location-service/src/main/java/de/healthIMIS/iris/irislocationservice/IrisLocationServerApplication.java
@@ -0,0 +1,13 @@
+package de.healthIMIS.iris.irislocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisLocationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrisLocationServerApplication.class, args);
+	}
+
+}
