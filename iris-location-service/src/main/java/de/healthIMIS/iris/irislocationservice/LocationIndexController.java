058f90477130dc279f46709928bce388a5fe7d3e
Kai Greshake <development@kai-greshake.de>
2021-03-27 12:51:19 +0100

    Add placeholder files for location service functionality

---
+++ iris-location-service/src/main/java/de/healthIMIS/iris/irislocationservice/LocationIndexController.java
@@ -0,0 +1,11 @@
+/*
+ * Controller of the location index. Handles adding/removing entries from the index as well as search
+ *
+ * @author Kai Greshake
+ */
+
+
+@RestController
+public class LocationIndexController  {
+
+}
