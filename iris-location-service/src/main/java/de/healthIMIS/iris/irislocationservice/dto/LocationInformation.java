133e55380e33159bdeecd3f8412d1d21f58f07f8
Finke, Andreas <andreas.finke@payactive.eu>
2021-03-28 15:08:12 +0100

    debug

--- iris-location-service/src/main/java/de/healthIMIS/iris/irislocationservice/dto/LocationInformation.java
+++ iris-location-service/src/main/java/de/healthIMIS/iris/irislocationservice/dto/LocationInformation.java
@@ -2,6 +2,9 @@ package de.healthIMIS.iris.irislocationservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
@@ -17,6 +20,9 @@ import java.util.Objects;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-03-27T12:26:11.318Z[GMT]")
 
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class LocationInformation   {
   @JsonProperty("id")
   private String id = null;
@@ -34,157 +40,4 @@ public class LocationInformation   {
   @Valid
   private List<LocationContext> contexts = null;
 
-  public LocationInformation id(String id) {
-    this.id = id;
-    return this;
-  }
-
-  /**
-   * Interne ID (beim Provider)
-   * @return id
-   **/
-  @Schema(example = "5eddd61036d39a0ff8b11fdb", required = true, description = "Interne ID (beim Provider)")
-      @NotNull
-
-    public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public LocationInformation name(String name) {
-    this.name = name;
-    return this;
-  }
-
-  /**
-   * Name des Standorts
-   * @return name
-   **/
-  @Schema(example = "Restaurant Alberts", required = true, description = "Name des Standorts")
-      @NotNull
-
-    public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public LocationInformation publicKey(String publicKey) {
-    this.publicKey = publicKey;
-    return this;
-  }
-
-  /**
-   * Öffentlicher Schlüssel, ggf. für Nachrichtenaustausch
-   * @return publicKey
-   **/
-  @Schema(description = "Öffentlicher Schlüssel, ggf. für Nachrichtenaustausch")
-  
-    public String getPublicKey() {
-    return publicKey;
-  }
-
-  public void setPublicKey(String publicKey) {
-    this.publicKey = publicKey;
-  }
-
-  public LocationInformation contact(LocationContact contact) {
-    this.contact = contact;
-    return this;
-  }
-
-  /**
-   * Get contact
-   * @return contact
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    @Valid
-    public LocationContact getContact() {
-    return contact;
-  }
-
-  public void setContact(LocationContact contact) {
-    this.contact = contact;
-  }
-
-  public LocationInformation contexts(List<LocationContext> contexts) {
-    this.contexts = contexts;
-    return this;
-  }
-
-  public LocationInformation addContextsItem(LocationContext contextsItem) {
-    if (this.contexts == null) {
-      this.contexts = new ArrayList<LocationContext>();
-    }
-    this.contexts.add(contextsItem);
-    return this;
-  }
-
-  /**
-   * Get contexts
-   * @return contexts
-   **/
-  @Schema(description = "")
-      @Valid
-    public List<LocationContext> getContexts() {
-    return contexts;
-  }
-
-  public void setContexts(List<LocationContext> contexts) {
-    this.contexts = contexts;
-  }
-
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    LocationInformation locationInformation = (LocationInformation) o;
-    return Objects.equals(this.id, locationInformation.id) &&
-        Objects.equals(this.name, locationInformation.name) &&
-        Objects.equals(this.publicKey, locationInformation.publicKey) &&
-        Objects.equals(this.contact, locationInformation.contact) &&
-        Objects.equals(this.contexts, locationInformation.contexts);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name, publicKey, contact, contexts);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class LocationInformation {\n");
-    
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("    name: ").append(toIndentedString(name)).append("\n");
-    sb.append("    publicKey: ").append(toIndentedString(publicKey)).append("\n");
-    sb.append("    contact: ").append(toIndentedString(contact)).append("\n");
-    sb.append("    contexts: ").append(toIndentedString(contexts)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
