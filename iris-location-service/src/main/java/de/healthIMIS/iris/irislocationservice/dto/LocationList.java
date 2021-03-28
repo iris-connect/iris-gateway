d1c5f164d10c7bb63ee527ca5e439a3b362d8f76
Finke, Andreas <andreas.finke@payactive.eu>
2021-03-28 15:08:12 +0100

    debug

--- iris-location-service/src/main/java/de/healthIMIS/iris/irislocationservice/dto/LocationList.java
+++ iris-location-service/src/main/java/de/healthIMIS/iris/irislocationservice/dto/LocationList.java
@@ -3,6 +3,7 @@ package de.healthIMIS.iris.irislocationservice.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
@@ -22,73 +23,10 @@ import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class LocationList   {
-  @JsonProperty("locations")
-  @Setter
-  @Valid
+
   private List<LocationInformation> locations = new ArrayList<LocationInformation>();
 
-  public LocationList locations(List<LocationInformation> locations) {
-    this.locations = locations;
-    return this;
-  }
-
-  public LocationList addLocationsItem(LocationInformation locationsItem) {
-    this.locations.add(locationsItem);
-    return this;
-  }
-
-  /**
-   * Get locations
-   * @return locations
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-    @Valid
-    public List<LocationInformation> getLocations() {
-    return locations;
-  }
-
-  public void setLocations(List<LocationInformation> locations) {
-    this.locations = locations;
-  }
-
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    LocationList locationList = (LocationList) o;
-    return Objects.equals(this.locations, locationList.locations);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(locations);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class LocationList {\n");
-    
-    sb.append("    locations: ").append(toIndentedString(locations)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
