5739768ee201012124709a6ee73fbf59c1500c40
Kai Greshake <development@kai-greshake.de>
2021-03-28 10:16:20 +0200

    Implement basic location index service

--- iris-location-service/src/main/java/de/healthIMIS/iris/irislocationservice/dto/LocationList.java
+++ iris-location-service/src/main/java/de/healthIMIS/iris/irislocationservice/dto/LocationList.java
@@ -2,6 +2,7 @@ package de.healthIMIS.iris.irislocationservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
@@ -17,6 +18,7 @@ import java.util.Objects;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-03-27T12:26:11.318Z[GMT]")
 
 
+@AllArgsConstructor
 public class LocationList   {
   @JsonProperty("locations")
   @Valid
