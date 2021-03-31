0217831108238ee370d1b3a4ef7e5710941f2c05
Claudio Bianucci <claudio.bianucci@lionmint.com>
2021-03-31 18:25:08 +0200

    [feature] Submit data requests to app provider

---
+++ iris-public-server/src/main/java/de/healthIMIS/iris/public_server/config/RestTemplateConfig.java
@@ -0,0 +1,30 @@
+package de.healthIMIS.iris.public_server.config;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig{
+
+  @Bean
+  public RestTemplate restTemplate()
+      throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+
+    var sslContext = new SSLContextBuilder().loadTrustMaterial((chain, authType) -> true) // trust all server
+        // certificates
+        .build();
+    var socketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+    var httpClient = HttpClientBuilder.create().setSSLSocketFactory(socketFactory).build();
+    return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+  }
+
+}
