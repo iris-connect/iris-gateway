f6e204c07628b742c97fbb0e8b5545f316c5613d
Finke, Andreas <andreas.finke@gmail.com>
2021-03-31 18:12:45 +0100

    Adds docker build. We need to use Resource Streams to access JSON files in FatJar

--- iris-app-api-demo/src/main/java/de/healthIMIS/irisappapidemo/datasubmission/bootstrap/GuestLoader.java
+++ iris-app-api-demo/src/main/java/de/healthIMIS/irisappapidemo/datasubmission/bootstrap/GuestLoader.java
@@ -3,10 +3,12 @@ package de.healthIMIS.irisappapidemo.datasubmission.bootstrap;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import de.healthIMIS.irisappapidemo.config.ResourceHelper;
 import de.healthIMIS.irisappapidemo.datasubmission.model.dto.GuestDto;
 import de.healthIMIS.irisappapidemo.datasubmission.model.dto.GuestListDto;
 import de.healthIMIS.irisappapidemo.searchindex.model.LocationDto;
 import de.healthIMIS.irisappapidemo.searchindex.model.LocationsDto;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
@@ -19,16 +21,19 @@ import java.util.List;
 @Component
 public class GuestLoader {
 
+    @NonNull private ResourceHelper resourceHelper;
+    @NonNull private ObjectMapper objectMapper;
+
+
     @SneakyThrows
     public List<GuestDto> getGuests() {
 
         List<GuestDto> guests = new ArrayList<>();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("src/main/resources/bootstrap/guests/guest_list.json");
+        var resource = resourceHelper.loadResource("classpath:bootstrap/guests/guest_list.json");
 
         guests = objectMapper.registerModule(new JavaTimeModule()).readValue(
-                        file, new TypeReference<List<GuestDto>>(){});
+                resource.getInputStream(), new TypeReference<List<GuestDto>>(){});
 
         return guests;
     }
