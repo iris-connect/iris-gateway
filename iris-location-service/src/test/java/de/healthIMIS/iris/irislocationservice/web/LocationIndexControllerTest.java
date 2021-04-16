d3aa053aa3e9c4574b05f344ce3d8e89f6e9c47b
Jens Kutzsche <github@gebea.de>
2021-04-16 09:53:23 +0200

    iris-gateway/iris-backlog#48 fixes test errors

--- iris-location-service/src/test/java/de/healthIMIS/iris/irislocationservice/web/LocationIndexControllerTest.java
+++ iris-location-service/src/test/java/de/healthIMIS/iris/irislocationservice/web/LocationIndexControllerTest.java
@@ -1,7 +1,9 @@
 package de.healthIMIS.iris.irislocationservice.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.junit.jupiter.api.Assertions.*;
+
 import de.healthIMIS.iris.irislocationservice.dto.LocationList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,51 +14,52 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @WebAppConfiguration
 @AutoConfigureMockMvc
 class LocationIndexControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper om;
+	@Autowired
+	private ObjectMapper om;
 
+	@Test
+	public void testPost() throws Exception {
+		// when, then, assert
+		postLocations();
+	}
 
-    @Test
-    public void testPost() throws Exception {
-        // when, then, assert
-        postLocations();
-    }
+	@Test
+	public void testSearch() throws Exception {
+		postLocations();
+		var res = mockMvc.perform(MockMvcRequestBuilders.get("/search/Restaurant")
+				.header("x-provider-id", "f002f370-bd54-4325-ad91-1aff3bf730a5"))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
 
-    @Test
-    public void testSearch() throws Exception {
-        postLocations();
-        var res = mockMvc.perform(MockMvcRequestBuilders.get("/search/Restaurant")
-        ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+		var json = om.readValue(res.getResponse().getContentAsString(), LocationList.class);
 
-        var json = om.readValue(res.getResponse().getContentAsString(), LocationList.class);
+		assertEquals(1, json.getLocations().size());
+	}
 
-        assertEquals(1, json.getLocations().size());
-    }
+	@Test
+	public void testDelete() throws Exception {
+		postLocations();
 
-    @Test
-    public void testDelete() throws Exception {
-        postLocations();
+		mockMvc.perform(MockMvcRequestBuilders.delete("/search-index/locations/5eddd61036d39a0ff8b11fdb")
+				.header("x-provider-id", "f002f370-bd54-4325-ad91-1aff3bf730a5"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/search-index/locations/5eddd61036d39a0ff8b11fdb"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-
-    }
-
-    private void postLocations() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.put("/search-index/locations")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(TestData.LOCATIONS)
-        ).andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-    }
+	private void postLocations() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.put("/search-index/locations")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(TestData.LOCATIONS)
+				.header("x-provider-id", "f002f370-bd54-4325-ad91-1aff3bf730a5"))
+				.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+	}
 
 }
