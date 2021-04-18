55795b9eb19e1a67fab3f81fae5d4c2db352e55c
Finke, Andreas <andreas.finke@gmail.com>
2021-04-18 13:40:34 +0100

    #29 Changes ID from Long to UUID

--- iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/auth/db/model/UserAccountsRepository.java
+++ iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/auth/db/model/UserAccountsRepository.java
@@ -4,8 +4,9 @@ import de.healthIMIS.iris.client.data_request.DataRequest;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserAccountsRepository extends CrudRepository<UserAccount, Long> {
+public interface UserAccountsRepository extends CrudRepository<UserAccount, UUID> {
 
     Optional<UserAccount> findByUserName(String userName);
 
