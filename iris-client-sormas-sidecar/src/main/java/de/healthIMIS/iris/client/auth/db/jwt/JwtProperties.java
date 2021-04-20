71de219bb1e7e3dfe2ad415101d7a0da36873119
Jens Kutzsche <github@gebea.de>
2021-04-20 14:38:09 +0200

    #29 no default shared secret for prod profile for safety

--- iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/auth/db/jwt/JwtProperties.java
+++ iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/auth/db/jwt/JwtProperties.java
@@ -1,13 +1,8 @@
 package de.healthIMIS.iris.client.auth.db.jwt;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-import javax.validation.constraints.NotNull;
 
 @Data
 @ConfigurationProperties(prefix = "security.jwt")
@@ -17,6 +12,6 @@ import javax.validation.constraints.NotNull;
 )
 public class JwtProperties {
 
-  private String jwtSharedSecret;
+  private @NonNull String jwtSharedSecret;
 
 }
