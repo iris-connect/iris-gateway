fc0e8ca821328cbde3ef1a4a55c6199205108a9a
SWlionmint <sebastian.wegener@lionmint.com>
2021-04-27 17:46:13 +0200

    Adds json files to hold testdata and adjust null checks

--- iris-client-sormas-sidecar/src/test/java/de/healthIMIS/iris/client/search_client/SearchClientTests.java
+++ iris-client-sormas-sidecar/src/test/java/de/healthIMIS/iris/client/search_client/SearchClientTests.java
@@ -3,7 +3,18 @@ package de.healthIMIS.iris.client.search_client;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
 import de.healthIMIS.iris.client.IrisWireMockTest;
 import lombok.RequiredArgsConstructor;
 import wiremock.com.fasterxml.jackson.core.JsonProcessingException;
@@ -26,11 +37,17 @@ public class SearchClientTests {
 	private final String LOCATION_ID = "locationId";
 	private final String NAME = "name";
 	
+	@Autowired
+	private ResourceLoader resourceLoader;
+	
 	@Test
-	public void searchByKeywordReturnsLocation() throws JsonMappingException, JsonProcessingException {		
+	public void searchByKeywordReturnsLocation() throws IOException {		
 		// given
 		String searchPath = SEARCH_PATH + SEARCH_KEY;
-		String json = "{\"locations\": [{\"id\": \"" + LOCATION_ID + "\", \"providerId\": \"" + PROVIDER_ID + "\", \"name\": \"" + NAME + "\"}]}";
+		
+		Resource resource = resourceLoader.getResource("classpath:testdata/SearchClientSearchByKeywordReturnsLocation.json");
+		InputStream jsonStream = resource.getInputStream();
+		String json = new String(jsonStream.readAllBytes());
 		
 		var locItem = new LocationInformation().id(LOCATION_ID).name("name").providerId(PROVIDER_ID);
 		var locList = new LocationList().addLocationsItem(locItem);
@@ -51,10 +68,34 @@ public class SearchClientTests {
 	}
 	
 	@Test
-	public void searchByIdReturnsLocation() throws JsonMappingException, JsonProcessingException {
+	public void searchByIdReturnsLocation() throws IOException {
+		// given
+		String searchPath = SEARCH_PATH + PROVIDER_ID + "/" + LOCATION_ID;		
+		Resource resource = resourceLoader.getResource("classpath:testdata/SearchClientSearchByIdReturnsLocation.json");
+		InputStream jsonStream = resource.getInputStream();
+		String json = new String(jsonStream.readAllBytes());
+		
+		var locItem = new LocationInformation().id(LOCATION_ID).name(NAME).providerId(PROVIDER_ID);
+
+		stubFor(get(urlEqualTo(searchPath)).willReturn(aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(json)));
+		
+		// do
+		Location searchLocation = systemUnderTest.findByProviderIdAndLocationId(PROVIDER_ID, LOCATION_ID);
+		
+		// then
+		assertNotNull(searchLocation);
+		assertEquals(searchLocation.getLocationId(), locItem.getId());
+		assertEquals(searchLocation.getProviderId(), locItem.getProviderId());
+		assertEquals(searchLocation.getName(), locItem.getName());
+	}
+	
+	@Test
+	public void searchByIdReturnsLocationWithoutLocationIdAndProviderId() throws IOException {
 		// given
 		String searchPath = SEARCH_PATH + PROVIDER_ID + "/" + LOCATION_ID;
-		String json = "{\"id\": \"" + LOCATION_ID + "\", \"providerId\": \"" + PROVIDER_ID + "\", \"name\": \"" + NAME + "\"}";
+		Resource resource = resourceLoader.getResource("classpath:testdata/SearchClientSearchByIdReturnsLocationWithoutLocationIdAndProviderId.json");
+		InputStream jsonStream = resource.getInputStream();
+		String json = new String(jsonStream.readAllBytes());
 		
 		var locItem = new LocationInformation().id(LOCATION_ID).name(NAME).providerId(PROVIDER_ID);
 
