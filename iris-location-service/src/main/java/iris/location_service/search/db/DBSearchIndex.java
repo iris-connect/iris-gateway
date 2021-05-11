096ebbf06dfe363798f75835b4f1e0ba25771a4c
Sebastian Wegener <sebastian.wegener@lionmint.com>
2021-05-11 14:02:34 +0200

    fix: #84 Adjust implementation to ignore cases sensitivity for location searches

--- iris-location-service/src/main/java/iris/location_service/search/db/DBSearchIndex.java
+++ iris-location-service/src/main/java/iris/location_service/search/db/DBSearchIndex.java
@@ -4,32 +4,29 @@ import iris.location_service.dto.LocationInformation;
 import iris.location_service.search.SearchIndex;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class DBSearchIndex implements SearchIndex {
 
-    @Getter
-    private final LocationRepository repo;
+	@Getter
+	private final LocationRepository repo;
 
-    private final ModelMapper mapper;
+	private final ModelMapper mapper;
 
-    @Override
-    public List<LocationInformation> search(String keyword) {
-
-        return repo.findByNameContaining(keyword).stream()
-                .limit(20)
-                .map(res -> {
-                    var location = mapper.map(res, LocationInformation.class);
-                    location.setId(res.getId().getLocationId());
-                    location.setProviderId(res.getId().getProviderId());
-                    return location;
-                }).collect(Collectors.toList());
-
-    }
+	@Override
+	public List<LocationInformation> search(String keyword) {
+		return repo.findByNameContainingIgnoreCase(keyword).stream().limit(20).map(res -> {
+			var location = mapper.map(res, LocationInformation.class);
+			location.setId(res.getId().getLocationId());
+			location.setProviderId(res.getId().getProviderId());
+			return location;
+		}).collect(Collectors.toList());
+	}
 }
--- iris-location-service/src/main/java/iris/location_service/search/db/LocationRepository.java
+++ iris-location-service/src/main/java/iris/location_service/search/db/LocationRepository.java
@@ -1,16 +1,15 @@
 package iris.location_service.search.db;
 
-
-import org.springframework.data.repository.CrudRepository;
-
 import iris.location_service.search.db.model.Location;
 import iris.location_service.search.db.model.LocationIdentifier;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
 public interface LocationRepository extends CrudRepository<Location, LocationIdentifier> {
-    List<Location> findByNameContaining(String query);
 
-    List<Location> findByIdProviderId(String providerId);
+	List<Location> findByNameContainingIgnoreCase(String query);
+
+	List<Location> findByIdProviderId(String providerId);
 }
-
