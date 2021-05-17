77f17656f1feb2c8aacf4fc48fe56bea8d94e8e0
Tim <62595633+lucky-lusa@users.noreply.github.com>
2021-05-17 17:46:00 +0200

    removed providerId from

--- demo-checkin-app/src/main/java/iris/demo/checkin_app/searchindex/eps/dto/DeleteLocationDTO.java
+++ demo-checkin-app/src/main/java/iris/demo/checkin_app/searchindex/eps/dto/DeleteLocationDTO.java
@@ -1,18 +1,13 @@
 package iris.demo.checkin_app.searchindex.eps.dto;
 
-import iris.demo.checkin_app.searchindex.model.LocationDto;
-import iris.demo.checkin_app.searchindex.model.LocationsDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class DeleteLocationDTO {
 
-    private String providerId;
-    private String locationId;
+	private String locationId;
 }
