4547376261b99fd11b6fbf67c2042c45971e0453
Fabian <fa.schroeder@ostfalia.de>
2021-06-02 13:48:24 +0200

    added test cases for the LuceneIndexService

--- iris-location-service/src/test/java/iris/location_service/search/lucene/LuceneIndexServiceTest.java
+++ iris-location-service/src/test/java/iris/location_service/search/lucene/LuceneIndexServiceTest.java
@@ -1,9 +1,21 @@
 package iris.location_service.search.lucene;
 
+import iris.location_service.search.db.model.Location;
+import iris.location_service.search.db.model.LocationIdentifier;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,21 +24,48 @@ class LuceneIndexServiceTest {
 
     @Autowired
     private LuceneIndexService luceneIndexService;
+    @Autowired
+    private LuceneIndexServiceProperties luceneIndexServiceProperties;
 
-    @BeforeEach
+    @BeforeTestExecution
     public void setUp() {
         luceneIndexService.postConstruct();
     }
 
     @Test
-    void searchIsEmptyIfNoDocumentsPresent() {
-        assertEquals( 0, luceneIndexService.search("hola").size());
+    public void testWriter() throws Exception{
+        int testExpected =  FSDirectory.open(Paths.get(luceneIndexServiceProperties.getIndexDirectory())).listAll().length;
+        indexDocument();
+        int testValue = FSDirectory.open(Paths.get(luceneIndexServiceProperties.getIndexDirectory())).listAll().length;
+        assertTrue(testExpected<=testValue);
     }
 
+    /**
+     * Testing if the search for vwxyz is empty
+     */
     @Test
-    void searchListIsNotEmptyIfDocumentsArePresentAndKeyWordMatches() {
-        // Add documents
-        assertEquals( 0, luceneIndexService.search("hola").size());
+    void searchIsEmptyIfNoDocumentsPresent() {
+        assertEquals( 0, luceneIndexService.search("vwxyz").size());
     }
+
+    /**
+     * Testing if the search for the previous indexed location is defined
+     */
+    @Test
+    void searchListIsNotEmptyIfDocumentsArePresentAndKeyWordMatches() throws Exception {
+        indexDocument();
+        assertTrue( 1<= luceneIndexService.search("pablo").size());
+    }
+
+    /**
+     * creates a Document
+     * @throws Exception
+     */
+    void indexDocument() throws Exception {
+        Directory dir = FSDirectory.open(Paths.get(luceneIndexServiceProperties.getIndexDirectory()));
+        Location testObject = new Location(new LocationIdentifier("123","123"),"Pablo","Pablo Hun", "Pablo","Rom 1", "Rom", "12345","pablo.h@test.com","pablotest@test.de","01234 1512435");
+        luceneIndexService.indexLocation(testObject);
+    }
+
 
 }
