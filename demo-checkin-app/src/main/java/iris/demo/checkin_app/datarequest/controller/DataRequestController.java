08bbc00f5256dfbe29260e5f07726ee5b9c46efc
Jens Kutzsche <github@gebea.de>
2021-07-07 15:09:17 +0200

    fix: fixes security error "Information exposure through a stack trace"
    Ref https://github.com/iris-connect/iris-gateway/security/code-scanning/1

--- demo-checkin-app/src/main/java/iris/demo/checkin_app/datarequest/controller/DataRequestController.java
+++ demo-checkin-app/src/main/java/iris/demo/checkin_app/datarequest/controller/DataRequestController.java
@@ -11,27 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import iris.demo.checkin_app.datarequest.model.dto.LocationDataRequestDto;
 import iris.demo.checkin_app.datasubmission.service.DataSubmissionService;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/iris/data-request")
+@Slf4j
 public class DataRequestController {
 
     @Autowired
     private DataSubmissionService dataSubmissionService;
 
     @PostMapping
-    public ResponseEntity postDataRequest(@Valid @RequestBody LocationDataRequestDto locationDataRequestDto) {
-        HttpHeaders headers = new HttpHeaders();
+    public ResponseEntity<String> postDataRequest(@Valid @RequestBody LocationDataRequestDto locationDataRequestDto) {
+        var headers = new HttpHeaders();
 
         try {
             dataSubmissionService.sendDataForRequest(locationDataRequestDto);
         } catch (Exception e) {
-            return new ResponseEntity<String>(e.getMessage(), headers, HttpStatus.BAD_REQUEST);
+        	
+        	log.error(e.getMessage());
+        	
+        	return new ResponseEntity<>("Could not send data. The error was logged.", headers, HttpStatus.BAD_REQUEST);
         }
 
-        return new ResponseEntity(headers, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(headers, HttpStatus.ACCEPTED);
     }
 
 
