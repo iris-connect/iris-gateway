a79f04d6beb436c5d5da9e774b378af41c0cfe64
Timo Boegner <tboegner@googlemail.com>
2021-07-15 15:21:12 +0200

    chore: Cleanup ContactsLoader.

--- demo-contact-diary-app/src/main/java/iris/demo/contact_diary_app/submission/bootstrap/ContactsLoader.java
+++ demo-contact-diary-app/src/main/java/iris/demo/contact_diary_app/submission/bootstrap/ContactsLoader.java
@@ -1,7 +1,5 @@
 package iris.demo.contact_diary_app.submission.bootstrap;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import iris.demo.contact_diary_app.config.ResourceHelper;
 import iris.demo.contact_diary_app.submission.rpc.dto.Contacts;
 import iris.demo.contact_diary_app.utils.ObjectMapperWithTimeModule;
@@ -15,8 +13,7 @@ import org.springframework.stereotype.Component;
 public class ContactsLoader {
 
     @NonNull private final ResourceHelper resourceHelper;
-    //@NonNull private final ObjectMapperWithTimeModule objectMapper;
-    @NonNull private final ObjectMapper objectMapper;
+    @NonNull private final ObjectMapperWithTimeModule objectMapper;
 
 
     @SneakyThrows
@@ -24,7 +21,7 @@ public class ContactsLoader {
 
         var resource = resourceHelper.loadResource("classpath:bootstrap/contact_list.json");
 
-        return objectMapper.registerModule(new JavaTimeModule()).readValue(resource.getInputStream(), Contacts.class);
+        return objectMapper.readValue(resource.getInputStream(), Contacts.class);
     }
 
 }
