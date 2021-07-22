d7d67b586d5bd873a0ff121d922cd59a7e9cccd1
Sebastian Wegener <sebastian.wegener@lionmint.com>
2021-07-22 17:58:11 +0200

    feat: Refactor and fix tests
    Refs: iris-gateway/iris-backlog#145

--- iris-location-service/src/main/java/iris/location_service/jsonrpc/LocationRPCImpl.java
+++ iris-location-service/src/main/java/iris/location_service/jsonrpc/LocationRPCImpl.java
@@ -35,6 +35,19 @@ import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImpl;
 @Slf4j
 public class LocationRPCImpl implements LocationRPC {
 
+	private static final String EXCEPTION_MESSAGE_ZIP = " - zip: ";
+	private static final String EXCEPTION_MESSAGE_STREET = " - street: ";
+	private static final String EXCEPTION_MESSAGE_CITY = " - city: ";
+	private static final String EXCEPTION_MESSAGE_PHONE = " - phone: ";
+	private static final String EXCEPTION_MESSAGE_EMAIL = " - email: ";
+	private static final String EXCEPTION_MESSAGE_OWNER_EMAIL = " - ownerEmail: ";
+	private static final String EXCEPTION_MESSAGE_REPRESENTATIVE = " - representative: ";
+	private static final String EXCEPTION_MESSAGE_OFFICIAL_NAME = " - officialName: ";
+	private static final String EXCEPTION_MESSAGE_PUBLIC_KEY = " - publicKey: ";
+	private static final String EXCEPTION_MESSAGE_ID = " - id: ";
+	private static final String EXCEPTION_MESSAGE_NAME = " - name: ";
+	private static final String EXCEPTION_MESSAGE_LOCATION_ID = " - locationId: ";
+	private static final String EXCEPTION_MESSAGE_PROVIDER_ID = " - providerId: ";
 	private final @NotNull LocationService locationService;
 	private final @NotNull HealthEndpoint healthEndpoint;
 
@@ -66,14 +79,7 @@ public class LocationRPCImpl implements LocationRPC {
 	@Override
 	public List<LocationOverviewDto> getProviderLocations(JsonRpcClientDto client) {
 
-		if (client == null || client.getName() == null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE);
-		}
-
-		if (client.getName() != null && !ValidationHelper.isNotShowingSignsForAttacks(client.getName())) {
-			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - name: " + client.getName());
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE);
-		}
+		validateInputForGetProviderLocations(client);
 
 		var providerLocations = locationService.getProviderLocations(client.getName());
 
@@ -84,25 +90,7 @@ public class LocationRPCImpl implements LocationRPC {
 
 	@Override
 	public String deleteLocationFromSearchIndex(JsonRpcClientDto client, String locationId) {
-		boolean isInvalid = false;
-
-		if (client == null || client.getName() == null || locationId == null) {
-			isInvalid = true;
-		}
-
-		if (client != null && client.getName() != null && !ValidationHelper.isNotShowingSignsForAttacks(client.getName())) {
-			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - name: " + client.getName());
-			isInvalid = true;
-		}
-
-		if (!ValidationHelper.isNotShowingSignsForAttacks(locationId)) {
-			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - locationId: " + locationId);
-			isInvalid = true;
-		}
-
-		if (isInvalid) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE);
-		}
+		validateInputForDeleteLocationFromSearchIndex(client, locationId);
 
 		var ret = "NOT FOUND";
 
@@ -145,25 +133,7 @@ public class LocationRPCImpl implements LocationRPC {
 	@Override
 	public Object getLocationDetails(JsonRpcClientDto client, String providerId, String locationId) {
 
-		boolean isInvalid = false;
-
-		if (locationId == null || providerId == null) {
-			isInvalid = true;
-		}
-
-		if (providerId != null && !ValidationHelper.isNotShowingSignsForAttacks(providerId)) {
-			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - providerId: " + providerId);
-			isInvalid = true;
-		}
-
-		if (locationId != null && !ValidationHelper.isNotShowingSignsForAttacks(locationId)) {
-			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - locationId: " + locationId);
-			isInvalid = true;
-		}
-
-		if (isInvalid) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE);
-		}
+		validateInputForGetLocationDetails(providerId, locationId);
 
 		var locationInformation = locationService.getLocationByProviderIdAndLocationId(providerId, locationId);
 
@@ -178,6 +148,61 @@ public class LocationRPCImpl implements LocationRPC {
 			return locationInformation;
 
 		return "NOT FOUND";
+	}
+
+	private void validateInputForGetProviderLocations(JsonRpcClientDto client) {
+		if (client == null || client.getName() == null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE);
+		}
+
+		if (client.getName() != null && !ValidationHelper.isNotShowingSignsForAttacks(client.getName())) {
+			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_NAME + client.getName());
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE);
+		}
+	}
+
+	private void validateInputForDeleteLocationFromSearchIndex(JsonRpcClientDto client, String locationId) {
+		boolean isInvalid = false;
+
+		if (client == null || client.getName() == null || locationId == null) {
+			isInvalid = true;
+		}
+
+		if (client != null && client.getName() != null && !ValidationHelper.isNotShowingSignsForAttacks(client.getName())) {
+			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_NAME + client.getName());
+			isInvalid = true;
+		}
+
+		if (!ValidationHelper.isNotShowingSignsForAttacks(locationId)) {
+			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_LOCATION_ID + locationId);
+			isInvalid = true;
+		}
+
+		if (isInvalid) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE);
+		}
+	}
+
+	private void validateInputForGetLocationDetails(String providerId, String locationId) {
+		boolean isInvalid = false;
+
+		if (locationId == null || providerId == null) {
+			isInvalid = true;
+		}
+
+		if (providerId != null && !ValidationHelper.isNotShowingSignsForAttacks(providerId)) {
+			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_PROVIDER_ID + providerId);
+			isInvalid = true;
+		}
+
+		if (locationId != null && !ValidationHelper.isNotShowingSignsForAttacks(locationId)) {
+			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_LOCATION_ID + locationId);
+			isInvalid = true;
+		}
+
+		if (isInvalid) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE);
+		}
 	}
 
 	@Override
@@ -196,7 +221,7 @@ public class LocationRPCImpl implements LocationRPC {
 		}
 
 		if (client != null && client.getName() != null && !ValidationHelper.isNotShowingSignsForAttacks(client.getName())) {
-			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - providerId: " + client.getName());
+			log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_PROVIDER_ID + client.getName());
 			return false;
 		}
 
@@ -208,102 +233,94 @@ public class LocationRPCImpl implements LocationRPC {
 		List<LocationInformation> validatedLocationList = new ArrayList<LocationInformation>();
 
 		for (LocationInformation locationInformation : locationList) {
-			if (locationInformation.getId() != null) {
-				if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getId())) {
-					log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - id: " + locationInformation.getId());
-					locationInformation.setId(ErrorMessageHelper.INVALID_INPUT_STRING);
-				}
+			if (locationInformation.getId() != null && ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getId())) {
+				log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_ID + locationInformation.getId());
+				locationInformation.setId(ErrorMessageHelper.INVALID_INPUT_STRING);
 			}
 
-			if (locationInformation.getProviderId() != null) {
-				if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getProviderId())) {
-					log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - providerId: " + locationInformation.getProviderId());
-					locationInformation.setProviderId(ErrorMessageHelper.INVALID_INPUT_STRING);
-				}
+			if (locationInformation.getProviderId() != null && ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getProviderId())) {
+				log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_PROVIDER_ID + locationInformation.getProviderId());
+				locationInformation.setProviderId(ErrorMessageHelper.INVALID_INPUT_STRING);
 			}
 
-			if (locationInformation.getName() != null) {
-				if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getName())) {
-					log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - name: " + locationInformation.getName());
-					locationInformation.setName(ErrorMessageHelper.INVALID_INPUT_STRING);
-				}
+			if (locationInformation.getName() != null && ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getName())) {
+				log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_NAME + locationInformation.getName());
+				locationInformation.setName(ErrorMessageHelper.INVALID_INPUT_STRING);
 			}
 
-			if (locationInformation.getPublicKey() != null) {
-				if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getPublicKey())) {
-					log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - publicKey: " + locationInformation.getPublicKey());
-					locationInformation.setPublicKey(ErrorMessageHelper.INVALID_INPUT_STRING);
-				}
+			if (locationInformation.getPublicKey() != null && ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getPublicKey())) {
+				log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_PUBLIC_KEY + locationInformation.getPublicKey());
+				locationInformation.setPublicKey(ErrorMessageHelper.INVALID_INPUT_STRING);
 			}
 
 			if (locationInformation.getContact() != null) {
-				if (locationInformation.getContact().getOfficialName() != null) {
-					if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getOfficialName())) {
-						log.warn(
-							ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - officialName: "
-								+ locationInformation.getContact().getOfficialName());
-						locationInformation.getContact().setOfficialName(ErrorMessageHelper.INVALID_INPUT_STRING);
-					}
+				if (locationInformation.getContact().getOfficialName() != null
+					&& ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getOfficialName())) {
+					log.warn(
+						ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE
+							+ EXCEPTION_MESSAGE_OFFICIAL_NAME
+							+ locationInformation.getContact().getOfficialName());
+					locationInformation.getContact().setOfficialName(ErrorMessageHelper.INVALID_INPUT_STRING);
 				}
 
-				if (locationInformation.getContact().getRepresentative() != null) {
-					if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getRepresentative())) {
-						log.warn(
-							ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - representative: "
-								+ locationInformation.getContact().getRepresentative());
-						locationInformation.getContact().setRepresentative(ErrorMessageHelper.INVALID_INPUT_STRING);
-					}
+				if (locationInformation.getContact().getRepresentative() != null
+					&& ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getRepresentative())) {
+					log.warn(
+						ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE
+							+ EXCEPTION_MESSAGE_REPRESENTATIVE
+							+ locationInformation.getContact().getRepresentative());
+					locationInformation.getContact().setRepresentative(ErrorMessageHelper.INVALID_INPUT_STRING);
 				}
 
-				if (locationInformation.getContact().getOwnerEmail() != null) {
-					if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getOwnerEmail())) {
-						log.warn(
-							ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - ownerEmail: "
-								+ locationInformation.getContact().getOwnerEmail());
-						locationInformation.getContact().setOwnerEmail(ErrorMessageHelper.INVALID_INPUT_STRING);
-					}
+				if (locationInformation.getContact().getOwnerEmail() != null
+					&& ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getOwnerEmail())) {
+					log.warn(
+						ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE
+							+ EXCEPTION_MESSAGE_OWNER_EMAIL
+							+ locationInformation.getContact().getOwnerEmail());
+					locationInformation.getContact().setOwnerEmail(ErrorMessageHelper.INVALID_INPUT_STRING);
 				}
 
-				if (locationInformation.getContact().getEmail() != null) {
-					if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getEmail())) {
-						log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - email: " + locationInformation.getContact().getEmail());
-						locationInformation.getContact().setEmail(ErrorMessageHelper.INVALID_INPUT_STRING);
-					}
+				if (locationInformation.getContact().getEmail() != null
+					&& ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getEmail())) {
+					log.warn(
+						ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_EMAIL + locationInformation.getContact().getEmail());
+					locationInformation.getContact().setEmail(ErrorMessageHelper.INVALID_INPUT_STRING);
 				}
 
-				if (locationInformation.getContact().getPhone() != null) {
-					if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getPhone())) {
-						log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - phone: " + locationInformation.getContact().getPhone());
-						locationInformation.getContact().setPhone(ErrorMessageHelper.INVALID_INPUT_STRING);
-					}
+				if (locationInformation.getContact().getPhone() != null
+					&& ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getPhone())) {
+					log.warn(
+						ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_PHONE + locationInformation.getContact().getPhone());
+					locationInformation.getContact().setPhone(ErrorMessageHelper.INVALID_INPUT_STRING);
 				}
 
 				if (locationInformation.getContact().getAddress() != null) {
-					if (locationInformation.getContact().getAddress().getCity() != null) {
-						if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getAddress().getCity())) {
-							log.warn(
-								ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - city: "
-									+ locationInformation.getContact().getAddress().getCity());
-							locationInformation.getContact().getAddress().setCity(ErrorMessageHelper.INVALID_INPUT_STRING);
-						}
+					if (locationInformation.getContact().getAddress().getCity() != null
+						&& ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getAddress().getCity())) {
+						log.warn(
+							ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE
+								+ EXCEPTION_MESSAGE_CITY
+								+ locationInformation.getContact().getAddress().getCity());
+						locationInformation.getContact().getAddress().setCity(ErrorMessageHelper.INVALID_INPUT_STRING);
 					}
 
-					if (locationInformation.getContact().getAddress().getStreet() != null) {
-						if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getAddress().getStreet())) {
-							log.warn(
-								ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - street: "
-									+ locationInformation.getContact().getAddress().getStreet());
-							locationInformation.getContact().getAddress().setStreet(ErrorMessageHelper.INVALID_INPUT_STRING);
-						}
+					if (locationInformation.getContact().getAddress().getStreet() != null
+						&& ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getAddress().getStreet())) {
+						log.warn(
+							ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE
+								+ EXCEPTION_MESSAGE_STREET
+								+ locationInformation.getContact().getAddress().getStreet());
+						locationInformation.getContact().getAddress().setStreet(ErrorMessageHelper.INVALID_INPUT_STRING);
 					}
 
-					if (locationInformation.getContact().getAddress().getZip() != null) {
-						if (ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getAddress().getZip())) {
-							log.warn(
-								ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - zip: "
-									+ locationInformation.getContact().getAddress().getZip());
-							locationInformation.getContact().getAddress().setZip(ErrorMessageHelper.INVALID_INPUT_STRING);
-						}
+					if (locationInformation.getContact().getAddress().getZip() != null
+						&& ValidationHelper.isNotShowingSignsForAttacks(locationInformation.getContact().getAddress().getZip())) {
+						log.warn(
+							ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE
+								+ EXCEPTION_MESSAGE_ZIP
+								+ locationInformation.getContact().getAddress().getZip());
+						locationInformation.getContact().getAddress().setZip(ErrorMessageHelper.INVALID_INPUT_STRING);
 					}
 				}
 			}
@@ -312,18 +329,14 @@ public class LocationRPCImpl implements LocationRPC {
 				List<LocationContext> contextsValidated = new ArrayList<LocationContext>();
 
 				for (LocationContext locationContext : locationInformation.getContexts()) {
-					if (locationContext.getId() != null) {
-						if (ValidationHelper.isNotShowingSignsForAttacks(locationContext.getId())) {
-							log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - id: " + locationContext.getId());
-							locationContext.setId(ErrorMessageHelper.INVALID_INPUT_STRING);
-						}
+					if (locationContext.getId() != null && ValidationHelper.isNotShowingSignsForAttacks(locationContext.getId())) {
+						log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_ID + locationContext.getId());
+						locationContext.setId(ErrorMessageHelper.INVALID_INPUT_STRING);
 					}
 
-					if (locationContext.getName() != null) {
-						if (ValidationHelper.isNotShowingSignsForAttacks(locationContext.getName())) {
-							log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + " - name: " + locationContext.getName());
-							locationContext.setName(ErrorMessageHelper.INVALID_INPUT_STRING);
-						}
+					if (locationContext.getName() != null && ValidationHelper.isNotShowingSignsForAttacks(locationContext.getName())) {
+						log.warn(ErrorMessageHelper.INVALID_INPUT_EXCEPTION_MESSAGE + EXCEPTION_MESSAGE_NAME + locationContext.getName());
+						locationContext.setName(ErrorMessageHelper.INVALID_INPUT_STRING);
 					}
 
 					contextsValidated.add(locationContext);
