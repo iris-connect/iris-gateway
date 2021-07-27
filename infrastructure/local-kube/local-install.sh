53668d2856400481a47f6dcd1d08652430f530c2
Jens Kutzsche <github@gebea.de>
2021-07-27 17:10:24 +0200

    chore: adapts names in build scripts to the renamed backend service
    Refs  iris-connect/iris-backlog#199

--- infrastructure/local-kube/local-install.sh
+++ infrastructure/local-kube/local-install.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 cd ../../
 headTag=$(git rev-parse HEAD)
-if [[ "$(  docker images -f reference='inoeg/iris-location-service:'"$headTag"''  2> /dev/null)" == "" ]]; then
+if [[ "$(  docker images -f reference='inoeg/iris-backend-service:'"$headTag"''  2> /dev/null)" == "" ]]; then
   tag=$headTag
   echo "using local image with tag $tag"
   # try importing locally built images
-  k3d image import inoeg/iris-location-service:$tag
+  k3d image import inoeg/iris-backend-service:$tag
 else
   tag=$(git rev-parse origin/develop)
 fi
