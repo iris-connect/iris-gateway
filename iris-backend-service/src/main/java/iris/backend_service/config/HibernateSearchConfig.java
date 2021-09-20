ecb6d049c063143eadb8bbaf2875f0c92c6e104b
Jens Kutzsche <github@gebea.de>
2021-09-20 16:40:37 +0200

    feat: implements fuzzy and wildcard search for locations
    Integrates Hibernate Search for fuzzy and wildcard search of locations.
    Extends the location entity for search index and adapts the search
    method to use a Hibernate Search query for search.
    
    Refs iris-connect/iris-backlog#110

---
+++ iris-backend-service/src/main/java/iris/backend_service/config/HibernateSearchConfig.java
@@ -0,0 +1,96 @@
+package iris.backend_service.config;
+
+import iris.backend_service.locations.search.db.model.Location;
+import iris.backend_service.locations.search.db.model.LocationIdentifier;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
+import org.apache.lucene.analysis.snowball.SnowballPorterFilterFactory;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
+import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
+import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.mapping.HibernateOrmMappingConfigurationContext;
+import org.hibernate.search.mapper.orm.mapping.HibernateOrmSearchMappingConfigurer;
+import org.hibernate.search.mapper.orm.massindexing.MassIndexer;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.mapper.pojo.bridge.IdentifierBridge;
+import org.hibernate.search.mapper.pojo.bridge.runtime.IdentifierBridgeFromDocumentIdentifierContext;
+import org.hibernate.search.mapper.pojo.bridge.runtime.IdentifierBridgeToDocumentIdentifierContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class HibernateSearchConfig {
+
+	private final @NonNull EntityManagerFactory emf;
+
+	@Bean
+	SearchSession getSearchSession() {
+		return Search.session(emf.createEntityManager());
+	}
+
+	@PostConstruct
+	void initialize() throws InterruptedException {
+
+		MassIndexer indexer = getSearchSession().massIndexer(Location.class).threadsToLoadObjects(2);
+		try {
+			indexer.startAndWait();
+		} catch (InterruptedException e) {
+			log.error("Hibernate Search indexer was interrupted!", e);
+			throw e;
+		}
+	}
+
+	@Component("DocumentAnalysisConfigurer")
+	public static class DocumentAnalysisConfigurer implements LuceneAnalysisConfigurer {
+
+		@Override
+		public void configure(LuceneAnalysisConfigurationContext context) {
+			context.analyzer("german").custom()
+					.tokenizer(StandardTokenizerFactory.class)
+					.tokenFilter(LowerCaseFilterFactory.class)
+					.tokenFilter(SnowballPorterFilterFactory.class)
+					.param("language", "German")
+					.tokenFilter(ASCIIFoldingFilterFactory.class);
+		}
+	}
+
+	@Component("SearchMappingConfigurer")
+	public static class SearchMappingConfigurer implements HibernateOrmSearchMappingConfigurer {
+		@Override
+		public void configure(HibernateOrmMappingConfigurationContext context) {
+			context.bridges().exactType(LocationIdentifier.class).identifierBridge(new IdBridge());
+		}
+	}
+
+	static class IdBridge implements IdentifierBridge<LocationIdentifier> {
+
+		@Override
+		public String toDocumentIdentifier(LocationIdentifier value, IdentifierBridgeToDocumentIdentifierContext context) {
+			return value == null ? null : String.format("%s##%s", value.getProviderId(), value.getLocationId());
+		}
+
+		@Override
+		public LocationIdentifier fromDocumentIdentifier(String value,
+				IdentifierBridgeFromDocumentIdentifierContext context) {
+
+			if (value == null) {
+				return null;
+			}
+
+			var ids = value.split("##");
+
+			return new LocationIdentifier(ids[0], ids[1]);
+		}
+	}
+}
--- iris-backend-service/src/main/java/iris/backend_service/locations/search/db/DBSearchIndex.java
+++ iris-backend-service/src/main/java/iris/backend_service/locations/search/db/DBSearchIndex.java
@@ -2,11 +2,16 @@ package iris.backend_service.locations.search.db;
 
 import iris.backend_service.locations.dto.LocationInformation;
 import iris.backend_service.locations.search.SearchIndex;
+import iris.backend_service.locations.search.db.model.Location;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
+import java.util.stream.Collectors;
+
+import org.hibernate.search.engine.search.sort.dsl.SortOrder;
+import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +19,48 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class DBSearchIndex implements SearchIndex {
 
-  @Getter
-  private final LocationRepository locationRepo;
+	private static final String[] FIELDS = { "name_search", "officialName_search", "representative_search",
+			"street_search", "contactAddressCity", "contactAddressZip", "contactEmail", "contactPhone" };
 
-  private final ModelMapper mapper;
+	private final SearchSession searchSession;
+	private final ModelMapper mapper;
 
-  @Override
-  public Page<LocationInformation> search(String keyword, Pageable pageable) {
-	return locationRepo.searchLocations(keyword, pageable).map(res -> {
-	  var location = mapper.map(res, LocationInformation.class);
-	  location.setId(res.getId().getLocationId());
-	  location.setProviderId(res.getId().getProviderId());
-	  return location;
-	});
-  }
+	@Override
+	public Page<LocationInformation> search(String keyword, Pageable pageable) {
+
+		var result = searchSession.search(Location.class)
+				.where(f -> f.bool()
+						.should(f2 -> f2.match()
+								.fields(FIELDS)
+								.matching(keyword)
+								.fuzzy())
+						.should(f2 -> f2.wildcard()
+								.fields(FIELDS)
+								.matching(String.format("*%s*", keyword))))
+				.sort(f -> f.composite(b -> {
+					if (pageable != null) {
+						pageable.getSort().forEach(s -> {
+							b.add(f.field(s.getProperty()).order(s.isAscending() ? SortOrder.ASC : SortOrder.DESC));
+						});
+					}
+				}))
+				.fetch(
+						(int) pageable.getOffset(),
+						pageable.getPageSize());
+
+		var locations = result.hits().stream()
+				.map(this::toLocationInformation)
+				.collect(Collectors.toList());
+
+		return new PageImpl<>(locations, pageable, result.total().hitCount());
+	}
+
+	private LocationInformation toLocationInformation(Location res) {
+
+		var location = mapper.map(res, LocationInformation.class);
+		location.setId(res.getId().getLocationId());
+		location.setProviderId(res.getId().getProviderId());
+
+		return location;
+	}
 }
