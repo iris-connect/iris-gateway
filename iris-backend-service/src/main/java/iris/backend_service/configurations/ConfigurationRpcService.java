cb9a5020f999b0d9932502b184ea885483105213
Jens Kutzsche <github@gebea.de>
2021-12-02 14:44:31 +0100

    chore: fixes details of the central configuration resulting from the tests with the dev and test environments
    * The proxy subdomain is now that of the dev environment.
    * If no config was found, a fixed error code is returned.
    * If no config was found, an alert is sent so we can adjust the central configuration.
    
    PR #231

--- iris-backend-service/src/main/java/iris/backend_service/configurations/ConfigurationRpcService.java
+++ iris-backend-service/src/main/java/iris/backend_service/configurations/ConfigurationRpcService.java
@@ -4,10 +4,15 @@ import iris.backend_service.jsonrpc.JsonRpcClientDto;
 
 import javax.validation.Valid;
 
+import com.googlecode.jsonrpc4j.JsonRpcError;
+import com.googlecode.jsonrpc4j.JsonRpcErrors;
 import com.googlecode.jsonrpc4j.JsonRpcParam;
 
 public interface ConfigurationRpcService {
 
+	@JsonRpcErrors({
+			@JsonRpcError(exception = CentralConfigurationException.class, code = -1111)
+	})
 	Configuration getHdConfiguration(@Valid @JsonRpcParam(value = "_client") JsonRpcClientDto client);
 
 	record Configuration(String abbreviation, String proxyUrl, String catSalt, String datSalt, int catLength,
